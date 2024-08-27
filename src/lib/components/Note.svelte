30b23d91dc65afcc433282111cf9cc3c7eb86764
Angus-Paillaugue <angus.paillaugue40@gmail.com>
2024-08-27 15:42:25 +0200

    Updated note component

--- src/lib/components/Note.svelte
+++ src/lib/components/Note.svelte
@@ -71,12 +71,12 @@
           <Icon class="size-6" icon={iconNames[type]} />
           <span class="font-semibold capitalize">{type}</span>
         </div>
-        <p class="m-0 text-text-body dark:text-text-body-dark">{@render children()}</p>
+        <div class="text-text-body dark:text-text-body-dark noteText">{@render children()}</div>
       </div>
     {:else if style === 'custom'}
       <div class="flex flex-row items-center gap-2">
         <Icon class="size-6" icon={iconNames[type]} />
-        <p class="m-0 text-text-body dark:text-text-body-dark">{@render children()}</p>
+        <div class="text-text-body dark:text-text-body-dark noteText">{@render children()}</div>
       </div>
     {/if}
   </div>
@@ -87,3 +87,9 @@
       .join(', ')}
   </p>
 {/if}
+
+<style>
+  :global(.noteText p) {
+    margin: 0;
+  }
+</style>
